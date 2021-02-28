02d4dd9fe29d3e3e5060838e5c6bfc12bde0228b
Shanmuga Sabareesh <ssabareesh.qtech@gmail.com>
2021-02-28 23:03:24 +0530

    cors issue fix commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { json } = require('body-parser');
+const cors = require('cors');
 const { getEmployers } = require('./routes/employer_routes');
 const { login } = require('./routes/user_routes');
 
 const app = express();
+
+app.use(cors());
+
 app.use(json());
 app.use(getEmployers);
 app.use(login);
